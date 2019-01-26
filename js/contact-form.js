7d78ad9c7efc5095ac32adbadd97b9767d6592e5
Omer Farooq Ahmed <omerfarooqali@gmail.com>
2019-01-26 22:52:31 +0500

    contact form backend integration done

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,57 +1,82 @@
 $(window).on("load", function() {
-    "use strict";
+  "use strict";
 
+  //  Contact Form Working Functionality
 
-
-    //  Contact Form Working Functionality
-
-    $( "#submit_3" ).on( "click", function() {
-        var errors = "";
-
-        var user_name = document.getElementById("username");
-        var contact_email_address = document.getElementById("email");
-        if(user_name.value === ""){
-            errors+= 'Please provide your name.';
+  $("#submit_3").on("click", function() {
+    if (
+      $("#username").val().length > 0 &&
+      $("#email").val().length > 0 &&
+      $("#phone").val().length > 0
+    ) {
+      console.log("button clicked", document.getElementById("username").value);
+      $.ajax({
+        type: "POST",
+        crossOrigin: true,
+        url: "https://crescon-server-995a8.firebaseapp.com/workwithus",
+        //   contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: {
+          name: document.getElementById("username").value,
+          email: document.getElementById("email").value,
+          phone: document.getElementById("phone").value,
+          position: document.getElementById("position").value,
+          message: document.getElementById("description").value
+        },
+        success: result => {
+          console.log(result);
         }
-        else if(contact_email_address.value === ""){
-            errors+= 'Please provide an email address.';
-        }
+      });
+      toastr.success(
+        "Thank you for submitting your request. Allow us a week to get back to you."
+      );
+    } else {
+      toastr.error("Please enter the required fields and then click again");
+    }
+    // testing
 
+    // var errors = "";
 
-        if(errors)
-        {
-            document.getElementById("error").style.display = "block";
-            document.getElementById("error").innerHTML = errors;
-            return false;
-        }
+    // var user_name = document.getElementById("username");
+    // var contact_email_address = document.getElementById("email");
+    // if(user_name.value === ""){
+    //     errors+= 'Please provide your name.';
+    // }
+    // else if(contact_email_address.value === ""){
+    //     errors+= 'Please provide an email address.';
+    // }
 
-        else{
-            $.ajax({
-                type: "POST",
-                url: "process.php",
-                data: $("#contact_form_3").serialize(),
-                success: function(msg)
-                {
-                    if(msg === 'success')
-                    {
-                        document.getElementById("error").style.display = "none";
-                        document.getElementById("username").value = "";
-                        document.getElementById("email").value = "";
-                        document.getElementById("description").value = "";
+    // if(errors)
+    // {
+    //     document.getElementById("error").style.display = "block";
+    //     document.getElementById("error").innerHTML = errors;
+    //     return false;
+    // }
+
+    // else{
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "process.php",
+    //         data: $("#contact_form_3").serialize(),
+    //         success: function(msg)
+    //         {
+    //             if(msg === 'success')
+    //             {
+    //                 document.getElementById("error").style.display = "none";
+    //                 document.getElementById("username").value = "";
+    //                 document.getElementById("email").value = "";
+    //                 document.getElementById("description").value = "";
     //                      $("#contact_form_3").hide();
-                        document.getElementById("success").style.display = "block";
-                        document.getElementById("success").innerHTML = "Thank You! We'll contact you shortly.";
-                    }else{
-                        document.getElementById("error").style.display = "block";
-                        document.getElementById("error").innerHTML = "Oops! Something went wrong while prceeding.";
-                    }
-                }
+    //                 document.getElementById("success").style.display = "block";
+    //                 document.getElementById("success").innerHTML = "Thank You! We'll contact you shortly.";
+    //             }else{
+    //                 document.getElementById("error").style.display = "block";
+    //                 document.getElementById("error").innerHTML = "Oops! Something went wrong while prceeding.";
+    //             }
+    //         }
 
-            });
+    //     });
 
-        }
-    });
-
-
-
-}); 
+    // }
+  });
+});
