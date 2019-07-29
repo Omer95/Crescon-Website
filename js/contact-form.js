1e38774369d4ba3ea77027ec8939cf5bcb8f16ae
Omer Farooq Ahmed <omerfarooqali@gmail.com>
2019-07-29 17:36:15 +0500

    website complete

--- js/contact-form.js
+++ js/contact-form.js
@@ -79,4 +79,36 @@ $(window).on("load", function() {
 
     // }
   });
+  $("#quote_submit").on("click", function() {
+    if (
+      $("#qusername").val().length > 0 &&
+      $("#qemail").val().length > 0 &&
+      $("#qphone").val().length > 0
+    ) {
+      console.log("button clicked", document.getElementById("username").value);
+      $.ajax({
+        type: "POST",
+        crossOrigin: true,
+        url: "https://crescon-server-995a8.firebaseapp.com/quote",
+        //   contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: {
+          qname: document.getElementById("qusername").value,
+          qemail: document.getElementById("qemail").value,
+          qphone: document.getElementById("qphone").value,
+          company: document.getElementById("comapny").value,
+          work: document.getElementById("work").value,
+          details: document.getElementById("details").value
+        },
+        success: result => {
+          console.log(result);
+        }
+      });
+      toastr.success(
+        "Thank you for submitting your request. Allow us a week to get back to you."
+      );
+    } else {
+      toastr.error("Please enter the required fields and then click again");
+    }
+  });
 });
